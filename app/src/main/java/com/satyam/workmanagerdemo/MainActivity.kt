19abd38b0da2cecf44035c57fba07207618693c5
Satyam Prajapati <satyam7355pra@gmail.com>
2024-11-11 18:31:19 +0530

    Working on input and output

--- app/src/main/java/com/satyam/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/satyam/workmanagerdemo/MainActivity.kt
@@ -3,12 +3,15 @@ package com.satyam.workmanagerdemo
 import android.os.Bundle
 import android.text.style.UpdateLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import androidx.work.impl.WorkManagerImpl
@@ -16,6 +19,9 @@ import androidx.work.impl.WorkManagerImpl
 class MainActivity : AppCompatActivity() {
 
     lateinit var textView : TextView
+    companion object{
+        const val KEY_COUNT_VALUE = "key_count"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,11 +35,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOneTimeWorkRequest(){
         val workManager = WorkManager.getInstance(applicationContext)
-        val uploadRequest =OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+
+        val data :Data = Data.Builder().putInt(KEY_COUNT_VALUE,125).build()
+        val constraints = Constraints.Builder().setRequiresCharging(false).build()
+        val uploadRequest =OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .setInputData(data)
+            .build()
 
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id).observe(this , Observer{
             textView.text = it.state.toString()
+            if(it.state.isFinished){
+                val data = it.outputData
+                val message = data.getString(UploadWorker.KeyWorker)
+                Toast.makeText(this@MainActivity,message,Toast.LENGTH_SHORT).show()
+            }
         })
     }
 
--- app/src/main/java/com/satyam/workmanagerdemo/UploadWorker.kt
+++ app/src/main/java/com/satyam/workmanagerdemo/UploadWorker.kt
@@ -2,17 +2,27 @@ package com.satyam.workmanagerdemo
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    companion object{
+        const val KeyWorker = "key_worker"
+    }
     override fun doWork(): Result {
         try {
-            for (i in 0..600000) {
+            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE,0)
+            for (i in 0 until count) {
                 Log.i("MyTag", "Uploading : $i")
             }
 
-            return Result.success()
+            val time = SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
+            val currentDate = time.format(Date())
+            val outPutData = Data.Builder().putString(KeyWorker,currentDate).build()
+            return Result.success(outPutData)
 
         } catch (e: Exception) {
             return Result.failure()
